3d3b2ca3c1ab1db9c9420aa3078add5180734d5e
OmarMahmoud2002 <omarmahmoud.devv@gmail.com>
2025-05-27 05:09:03 +0300

    Add User model with validations and constraints

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_05_27_013447) do
+ActiveRecord::Schema[8.0].define(version: 2025_05_27_020638) do
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.string "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name", null: false
+    t.date "DOB", null: false
+    t.string "email", null: false
+    t.string "phone_number", null: false
+    t.string "address", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 end
